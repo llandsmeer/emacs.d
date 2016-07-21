610a5823b51bf79605228fcf401cc9cd6c50e86d
Lennart Landsmeer <lennart@landsmeer.email>
2016-07-22 01:55:15 +0200

    whitespace mode tabs and trailing

--- init.el
+++ init.el
@@ -44,7 +44,7 @@
 (setq inhibit-startup-screen t)
 (setq initial-scratch-message nil)
 (setq use-dialog-box nil)
-(setq-default show-trailing-whitespace t)
+(setq whitespace-style '(face tabs trailing tab tab-mark))
 
 (require 'python)
 (add-hook 'python-mode 'run-python)
